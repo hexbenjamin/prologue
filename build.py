6e59f2fe6ac9197ffa304c783f40757c1135fe78
hex benjamin <hex@hexbenjam.in>
2023-12-15 00:55:14 -0500

    edit run.py as build.py to move run command out

---
+++ build.py
@@ -0,0 +1,5 @@
+from os import system as cmd
+
+cmd("pip install -r requirements.txt")
+cmd("reflex init")
+# cmd("reflex run --env prod")
--- run.py
+++
@@ -1,4 +0,0 @@
-from os import system as cmd
-
-cmd("reflex init")
-cmd("reflex run --env prod")
