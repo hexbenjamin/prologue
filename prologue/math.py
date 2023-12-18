16367445439b2329e222fea41437cf3700049f42
hex benjamin <hex@hexbenjam.in>
2023-12-18 13:40:26 -0500

    migrate swipe calculations into math module

--- prologue/math.py
+++ prologue/math.py
@@ -7,3 +7,21 @@ def scale_min(value=CFG.grid.scale):
 
 def scale_max(value=CFG.grid.scale):
     return f"max({int(value)}vw, {int(value)}vh)"
+
+
+def calc_direction(start, end):
+    dx = end[0] - start[0]
+    dy = end[1] - start[1]
+    angle = math.atan2(dy, dx)
+    angle = math.degrees(angle)
+    angle = (angle + 360) % 360
+
+    directions = ["W", "NW", "N", "NE", "E", "SE", "S", "SW"]
+    index = round(angle / 45) % 8
+    return directions[index]
+
+
+def calc_distance(point1, point2):
+    x1, y1 = point1
+    x2, y2 = point2
+    return math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
--- prologue/state.py
+++ prologue/state.py
@@ -2,25 +2,7 @@ import math
 import reflex as rx
 
 from .config import CFG
-from .math import scale_min, scale_max
-
-
-def calc_direction(start, end):
-    dx = end[0] - start[0]
-    dy = end[1] - start[1]
-    angle = math.atan2(dy, dx)
-    angle = math.degrees(angle)
-    angle = (angle + 360) % 360
-
-    directions = ["W", "NW", "N", "NE", "E", "SE", "S", "SW"]
-    index = round(angle / 45) % 8
-    return directions[index]
-
-
-def calc_distance(point1, point2):
-    x1, y1 = point1
-    x2, y2 = point2
-    return math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+from .math import scale_min, scale_max, calc_direction, calc_distance
 
 
 class PrologueState(rx.State):
